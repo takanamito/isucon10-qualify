f206b3c8382598a67cff2747cd257ff660cb29ed
takanamito <takanamito0928@gmail.com>
2020-09-21 16:46:07 +0900

    Add index

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -17,7 +17,8 @@ CREATE TABLE isuumo.estate
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    popularity  INTEGER             NOT NULL,
+    rev_popularity  INTEGER AS (-popularity) NOT NULL
 );
 
 CREATE TABLE isuumo.chair
@@ -34,9 +35,24 @@ CREATE TABLE isuumo.chair
     features    VARCHAR(64)     NOT NULL,
     kind        VARCHAR(64)     NOT NULL,
     popularity  INTEGER         NOT NULL,
+    rev_popularity INTEGER AS (-popularity) NOT NULL,
     stock       INTEGER         NOT NULL
 );
 
-CREATE INDEX rent_index ON isuumo.estate (rent);
+CREATE INDEX door_height_index ON isuumo.estate (door_height);
+CREATE INDEX door_width_index ON isuumo.estate (door_width);
+CREATE INDEX door_height_rent_index ON isuumo.estate (door_height, rent);
+CREATE INDEX door_width_rent_index ON isuumo.estate (door_width, rent);
+CREATE INDEX rent_id_index ON isuumo.estate (rent, id);
+CREATE INDEX rev_popularity_id_rent_index ON isuumo.estate (rev_popularity, id, rent);
+CREATE INDEX latitute_longitude_index ON isuumo.estate (latitude, longitude);
+
 CREATE INDEX stock_index ON isuumo.chair (stock);
-CREATE INDEX price_id_index ON isuumo.chair (price,id);
+CREATE INDEX price_id_index ON isuumo.chair (price, id);
+CREATE INDEX popularity_index ON isuumo.chair (rev_popularity, id);
+CREATE INDEX price_stock_index ON isuumo.chair (price, stock);
+CREATE INDEX kind_stock_index ON isuumo.chair (kind, stock);
+CREATE INDEX height_stock_index ON isuumo.chair (height, stock);
+CREATE INDEX width_stock_index ON isuumo.chair (width, stock);
+CREATE INDEX color_stock_index ON isuumo.chair (color, stock);
+CREATE INDEX depth_stock_index ON isuumo.chair (depth, stock);
